a83290c991e423d19a0c9c3097cec455bef831f8
Joan Sebastian Hernandez Ladino <sebastian@playvox.com>
2021-04-10 15:09:14 -0500

    [Feat] Added initial project structure

---
+++ main.py
@@ -0,0 +1,30 @@
+from menu import draw_menu
+
+
+def get_action(option):
+    """This method obtains the option action
+
+    :return: (def) The action option
+    """
+    return {
+        '1': "Option 1"
+    }.get(option, None)
+
+
+def start_app():
+    """This method starts the app"""
+    run_app = True
+    while run_app:
+        draw_menu()
+        input_user = input("Choice a option: ")
+        user_action = get_action(input_user)
+
+        if not user_action:
+            print("Leaving the app thanks ...")
+            exit()
+
+        print(user_action)
+
+
+if __name__ == '__main__':
+    start_app()
---
+++ menu.py
@@ -0,0 +1,5 @@
+
+
+def draw_menu():
+    print("(1) Get a leap info \n")
+    print("(Any number outside the menu) Exit\n\n")
